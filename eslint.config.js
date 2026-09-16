1c91624362006137842f727a89d1c82fd1a144c9
sefahanan449-byte <sefahanan449@gmail.com>
2026-09-16 05:14:57 -0700

    chore: create Vite TypeScript starter

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+  eslintConfigPrettier,
+);
